b644aaa57bd002c76e9a2faf904c53615cc9fa24
liujunjiang <liujunjiang@szxhinfo.com>
2017-06-12 14:41:37 +0800

    update

--- angular-initialize.js
+++ angular-initialize.js
@@ -1,6 +1,44 @@
+"use strict";
+
+var _getPrototypeOf = require("babel-runtime/core-js/object/get-prototype-of");
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _classCallCheck2 = require("babel-runtime/helpers/classCallCheck");
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _possibleConstructorReturn2 = require("babel-runtime/helpers/possibleConstructorReturn");
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require("babel-runtime/helpers/inherits");
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 module.exports = function ngInitialize(target) {
-  target.prototype.initialize = function(args) {
-    angular && angular.forEach(args, (arg, i) => this[target.$inject[i]] = arg);
-    this.$scope && this.viewLoaded && typeof this.viewLoaded == 'function' && this.$scope.$on('$viewContentLoaded', this.viewLoaded.bind(this));
-  };
-}
+  return function (_target) {
+    (0, _inherits3.default)(_class, _target);
+
+    function _class() {
+      var _ref;
+
+      (0, _classCallCheck3.default)(this, _class);
+
+      for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+      }
+
+      var _this = (0, _possibleConstructorReturn3.default)(this, (_ref = _class.__proto__ || (0, _getPrototypeOf2.default)(_class)).call.apply(_ref, [this].concat(args)));
+
+      angular && angular.forEach(args, function (arg, i) {
+        return _this[target.$inject[i]] = arg;
+      });
+      return _this;
+    }
+
+    return _class;
+  }(target);
+};
