34f5fbd22e76fba0d5b4d7f1333037e8f9147cbb
liujunjiang <liujunjiang@szxhinfo.com>
2017-06-12 12:28:35 +0800

    fix bug

--- angular-initialize.js
+++ angular-initialize.js
@@ -1,5 +1,6 @@
 module.exports = function ngInitialize(target) {
   target.prototype.initialize = function(args) {
     angular && angular.forEach(args, (arg, i) => this[target.$inject[i]] = arg);
+    this.$scope && this.viewLoaded && typeof this.viewLoaded == 'function' && this.$scope.$on('$viewContentLoaded', this.viewLoaded.bind(this));
   };
 }
